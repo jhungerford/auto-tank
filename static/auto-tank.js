44465c4c54757ed00bf44b90e9fa9366b4ff6ae6
John Hungerford <jhungerford@gmail.com>
2019-05-12 12:32:04 -0600

    Handle key releases in the browser.

--- static/auto-tank.js
+++ static/auto-tank.js
@@ -8,15 +8,16 @@ const keys = {
   left: false,
   right: false
 };
+let directionStack = ['stop'];
 
 function keyDown(event) {
   if (event.key.includes("Arrow")) {
     let direction = event.key.replace("Arrow", "").toLowerCase();
     // Only send the direction once when the key is pressed - down event is fired while the key is held.
     if (!keys[direction]) {
+      directionStack.push(direction);
       keys[direction] = true;
-      http.open("POST", "/move", true);
-      http.send(direction);
+      postMove(direction);
     }
   }
 }
@@ -25,5 +26,20 @@ function keyUp(event) {
   if (event.key.includes("Arrow")) {
     let direction = event.key.replace("Arrow", "").toLowerCase();
     keys[direction] = false;
+
+    let changeDirection = directionStack[directionStack.length - 1] === direction;
+
+    directionStack = directionStack.filter(function (value) {
+      return value !== direction;
+    });
+
+    if (changeDirection) {
+      postMove(directionStack[directionStack.length - 1]);
+    }
   }
+}
+
+function postMove(direction) {
+  http.open("POST", "/move", true);
+  http.send(direction);
 }
